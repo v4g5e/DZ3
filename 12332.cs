d01d9cc769ba85bd690d779123078268c1d26e9c
NX6 <val.yaros@bk.ru>
2025-11-26 22:12:47 +0300

    Add files via upload

---
+++ 12332.cs
@@ -0,0 +1,249 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2
+            // Console.WriteLine("Введите значение переменной - p");
+            //double p = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"K = {Math.Log(Math.Pow(p, 2) + Math.Pow(y, 3)) + Math.Exp(p)}");
+
+            //3
+            //Console.WriteLine("Введите значение переменной - n");
+            //double n = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"G = {n * (y + 3.5) + Math.Sqrt(y)}");
+
+            // 4 
+            //Console.WriteLine("Введите значение переменной - a");
+            //double a = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - t");
+            //double t = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"D = {9.8 * Math.Pow(a, 2) + 5.52 * Math.Cos(Math.Pow(t, 5)}");
+
+            //5
+            // Console.WriteLine("Введите значение переменной - x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"L = {1.51 * Math.Cos(Math.Pow(x, 2)) + 2 * Math.Pow(x, 3)}");
+
+            //6
+            // Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"M = {Math.Cos(2 * y) + 3.6 * Math.Exp(x)}");
+
+            //7
+            //Console.WriteLine("Введите значение переменной - m");
+            //double m = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"N = {Math.Pow(m, 2) + 2.8 * Math.Abs(m) + 0.55}");
+
+            //8
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"T = {Math.Abs(6 * Math.Pow(y, 2) - 0.1 * y + 4)}");
+
+            //9
+            // Console.WriteLine("Введите значение переменной - x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($" V = {Math.Log(y + 0.95) + Math.Sin(Math.Pow(x, 4)}");
+
+            //10
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("Введите значение переменной - x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"U = {Math.Exp(y) + 7.355 * Math.Pow(k, 2) + Math.Pow(Math.Sin(x), 2)}");
+
+            //11
+            // Console.WriteLine("Введите значение переменной - x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"S = {9.756 * Math.Pow(y, 7) + 2 * Math.Tan(x)}");
+
+            //12
+            // Console.WriteLine("Введите значение переменной - t");
+            //double t = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("Введите значение переменной - x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"K = {7 * Math.Pow(t, 2) + 3 * Math.Sin(Math.Pow(x, 3)) + 9.2}");
+
+            //13
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"E = {Math.Abs(3 * Math.Pow(y, 2) + 0.5 * y + 4})");
+
+            //15
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($H = {Math.Sin(Math.Pow(y, 2)) - 2.8 * y + Math.Sqrt(Math.Abs(y)}");
+
+            //16
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($S = {Math.Sqrt(Math.Cos(4 * Math.Pow(y, 2)) + 7.151}");
+
+            //18
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"Z = {3 * Math.Pow(y, 2) + Math.Pow(Math.Sqrt(y), 3) + 1}");
+            //Console.ReadKey();
+
+            // 19
+            //Console.WriteLine("Введите значение переменной - n");
+            //double n = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - g");
+            //double g = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"F = {n * Math.Sqrt(Math.Pow(y, 3)) + 1.09 * g}");
+            //Console.ReadKey();
+
+            // 20
+            //Console.WriteLine("Введите значение переменной - t");
+            //double t = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - g");
+            //double g = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"U = {Math.Exp(t * y) + Math.Tan(g) * Math.Sqrt(y)}");
+            //Console.ReadKey();
+
+            // 21
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - h");
+            //double h = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"P = {y + 5.5 + 9.1 * Math.Pow(h, 3)}");
+            //Console.ReadKey();
+
+            // 22
+            //Console.WriteLine("Введите значение переменной - u");
+            //double u = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"T = {Math.Sin(2 * u) * (Math.Log(Math.Pow(y, 2)) + Math.Sqrt(x))}");
+            //Console.ReadKey();
+
+            // 23
+            //Console.WriteLine("Введите значение переменную - e");
+            //double e = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - f");
+            //double f = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"G = {Math.Pow(e, 2) + Math.Sin(f)}");
+            //Console.ReadKey();
+
+            // 24
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"E = {2 * Math.Sin(0.214 * Math.Pow(y, 5)) + 1}");
+            //Console.ReadKey();
+
+            // 25
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - f");
+            //double f = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"C = {Math.Exp(2 * y) + Math.Sin(Math.Pow(f, 2))}");
+            //Console.ReadKey();
+
+            // 26
+            //Console.WriteLine("Введите значение переменной - p");
+            //double p = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"Z = {Math.Sin(Math.Pow(p, 2) + Math.Pow(0.4, 3))}");
+            //Console.ReadKey();
+
+            // 27
+            //Console.WriteLine("Введите значение переменной - v");
+            //double v = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - e");
+            //double e = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - x");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"W = {1.03 * v + Math.Pow(e, 2) + Math.Tan(x)}");
+            //Console.ReadKey();
+
+            // 28
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - h");
+            //double h = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"T = {Math.Pow(Math.E, y * h) + Math.Sqrt(6.4 * y)}");
+
+            // 29
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"N = {3 * Math.Pow(y, 2) + Math.Sqrt(y + 1)}");
+            //Console.ReadKey();
+
+            // 30
+            //Console.WriteLine("Введите значение переменной - y");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите значение переменной - r");
+            //double r = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"W = {Math.Exp(y) + 7.2 * Math.Sin(r)}");
+            //Console.ReadKey();
+        }
+    }
+}
+  
